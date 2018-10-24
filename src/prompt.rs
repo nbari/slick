849dca1dec2825c2dbb68acbb48701b28f986fa5
nbari <nbari@tequila.io>
2018-10-24 16:50:47 +0200

    sync Wed Oct 24 16:50:47 CEST 2018

--- src/prompt.rs
+++ src/prompt.rs
@@ -3,7 +3,8 @@ use clap::{ArgMatches};
 const COMMAND_KEYMAP:&str = "vicmd";
 const NON_BREAKING_SPACE:&str = " ";
 const NO_ERROR:&str = "0";
-const PROMPT_COLOR:i32 = 5;
+const PROMPT_COLOR:i32 = 74;
+const PROMPT_SYMBOL_COLOR:i32 = 5;
 const PROMPT_ERROR_COLOR:i32 = 196;
 const PROMPT_SYMBOL:&str = "$";
 const PROMPT_VICMD_COLOR:i32 = 3;
@@ -18,11 +19,12 @@ pub fn display(sub_matches: &ArgMatches) {
         _ => PROMPT_SYMBOL,
     };
 
-    let prompt_color = match (symbol, last_return_code) {
+    let prompt_symbol_color = match (symbol, last_return_code) {
         (PROMPT_VICMD_SYMBOL, _) => PROMPT_VICMD_COLOR,
-        (_, NO_ERROR)            => PROMPT_COLOR,
+        (_, NO_ERROR)            => PROMPT_SYMBOL_COLOR,
         _                        => PROMPT_ERROR_COLOR,
     };
 
-    print!("%F{{{}}}{}%f{}", prompt_color, symbol, NON_BREAKING_SPACE)
+    // print!("%F{{{}}}{}%f{}", prompt_color, symbol, NON_BREAKING_SPACE)
+    print!("%F{{{}}}%~{}\n%F{{{}}}{}%f{}", PROMPT_COLOR, " [test sleep here]", prompt_symbol_color, symbol, NON_BREAKING_SPACE)
 }
