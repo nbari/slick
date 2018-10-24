b9519ef68a6adee05d070e0062bb9a4df06cb2ec
nbari <nbari@tequila.io>
2018-10-25 00:33:05 +0200

    sync Thu Oct 25 00:33:05 CEST 2018

--- src/precmd.rs
+++ src/precmd.rs
@@ -3,9 +3,11 @@ use std::thread;
 use std::time::Duration;
 
 pub fn display(_sub_matches: &ArgMatches) {
-    thread::sleep(Duration::from_secs(0));
-    let _ = thread::spawn(|| {
-    thread::sleep(Duration::from_secs(2));
-    println!("[{}] ", 3);
-    });
+    //thread::sleep(Duration::from_secs(0));
+    //let _ = thread::spawn(|| {
+    //thread::sleep(Duration::from_secs(2));
+    //print!("[{}] ", 3);
+    //});
+   //print!("%F{{74}}%~%f");
+   println!("--");
 }
