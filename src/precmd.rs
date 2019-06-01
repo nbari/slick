71a93b40c19676cc2eb440365ac274f78b5e2229
nbari <nbari@tequila.io>
2019-06-01 20:33:14 +0200

    sync Sat Jun  1 20:33:14 CEST 2019

--- src/precmd.rs
+++ src/precmd.rs
@@ -1,8 +1,11 @@
 use clap::ArgMatches;
-use git2::{self, Repository, StatusOptions};
+use git2::Repository;
+use pty::fork::*;
 use std::env;
+use std::io::Read;
+use std::process::Command;
 
-fn repo_status(r: &Repository) -> Option<String> {
+fn repo_status(_r: &Repository) -> Option<String> {
     let mut res = String::new();
     res.push_str("hello");
     Some(res)
@@ -16,5 +19,21 @@ pub fn display(_sub_matches: &ArgMatches) {
         Err(_e) => None,
     };
 
-    println!("{:?}", branch);
+    let fork = Fork::from_ptmx().unwrap();
+
+    if let Some(mut master) = fork.is_parent().ok() {
+        // Read output via PTY master
+        let mut output = String::new();
+
+        match master.read_to_string(&mut output) {
+            Ok(_nread) => println!("child tty is: {} {:?}", output.trim(), branch),
+            Err(e) => panic!("read error: {}", e),
+        }
+    } else {
+        // Child process just exec `tty`
+        Command::new("sleep")
+            .arg("3")
+            .status()
+            .expect("could not execute tty");
+    }
 }
