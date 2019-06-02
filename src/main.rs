c7933ac75bb8fb8787001d2ac5ff1ba480dabf7f
nbari <nbari@tequila.io>
2019-06-02 20:07:06 +0200

    sync Sun Jun  2 20:07:06 CEST 2019

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         .get_matches();
 
     match matches.subcommand() {
-        ("precmd", Some(sub_matches)) => precmd::display(sub_matches),
+        ("precmd", Some(_)) => precmd::display(),
         ("prompt", Some(sub_matches)) => prompt::display(sub_matches),
         _ => (),
     }
--- src/precmd.rs
+++ src/precmd.rs
@@ -1,20 +1,55 @@
-use clap::ArgMatches;
-use git2::Repository;
+use git2::{Repository, StatusOptions, StatusShow};
+use std::collections::HashMap;
 use std::env;
 
-fn repo_status(_r: &Repository) -> Option<String> {
-    let mut res = String::new();
-    res.push_str("hello");
-    Some(res)
+fn repo_status(repo: &Repository) {
+    let mut prompt = String::new();
+
+    match repo.head() {
+        Ok(head) => {
+            prompt.push_str(head.shorthand().unwrap_or("(no branch)"));
+        }
+        Err(_) => {
+            return;
+        }
+    }
+
+    let mut status_opt = StatusOptions::new();
+
+    status_opt
+        .show(StatusShow::IndexAndWorkdir)
+        .include_untracked(true)
+        .include_unmodified(false)
+        .no_refresh(false);
+
+    let status_result = repo.statuses(Some(&mut status_opt)).unwrap();
+
+    if status_result.len() == 0 {
+        print!("{}", prompt);
+        return;
+    }
+
+    //let mut map: HashMap<&str, u32> = HashMap::new();
+    let mut map: HashMap<git2::Status, u32> = HashMap::new();
+
+    for status_entry in status_result.iter() {
+        println!(
+            "branch: {} {:?} {:?}",
+            prompt,
+            status_entry.path(),
+            status_entry.status()
+        );
+        *map.entry(status_entry.status()).or_insert(0) += 1;
+    }
+    println!("{:?}", map);
 }
 
-pub fn display(_sub_matches: &ArgMatches) {
+pub fn display() {
     let path = env::current_dir().unwrap();
-
-    let branch = match Repository::discover(path) {
+    match Repository::discover(path) {
         Ok(repo) => repo_status(&repo),
-        Err(_e) => None,
-    };
-
-    println!("{:?}", branch);
+        Err(_) => {
+            return;
+        }
+    }
 }
