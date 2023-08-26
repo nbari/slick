6bb8bc1605e04df4acbdfaef9d27e857ae360df5
nbari <nbari@tequila.io>
2023-08-26 19:37:08 +0200

    clippy ## raw strings -- still need a cleanup

--- src/bin/slick.rs
+++ src/bin/slick.rs
@@ -14,7 +14,7 @@ fn main() {
             Command::new("prompt")
                 .about("prompt")
                 .override_help(
-                    r##"Builds the prompt, render is affected by this environment vars:
+                    r#"Builds the prompt, render is affected by this environment vars:
 
 The default values are:
 
@@ -25,12 +25,15 @@ The default values are:
     SLICK_PROMPT_GIT_FETCH=1 (if set to 0 disables git fetch)
     SLICK_PROMPT_GIT_MASTER_BRANCH_COLOR=160
     SLICK_PROMPT_GIT_REMOTE_COLOR=6
+    SLICK_PROMPT_GIT_REMOTE_AHEAD=⇡
+    SLICK_PROMPT_GIT_REMOTE_BEHIND=⇣
     SLICK_PROMPT_GIT_STAGED_COLOR=7
     SLICK_PROMPT_GIT_STATUS_COLOR=5
     SLICK_PROMPT_GIT_UNAME_COLOR=8
+    SLICK_PROMPT_NON_BREAKING_SPACE="\u{a0}"
     SLICK_PROMPT_PATH_COLOR=74
     SLICK_PROMPT_ROOT_COLOR=1
-    SLICK_PROMPT_ROOT_SYMBOL="#"
+    SLICK_PROMPT_ROOT_SYMBOL=#
     SLICK_PROMPT_SSH_COLOR=8
     SLICK_PROMPT_SYMBOL="$"
     SLICK_PROMPT_SYMBOL_COLOR=5
@@ -38,7 +41,7 @@ The default values are:
     SLICK_PROMPT_VICMD_COLOR=3
     SLICK_PROMPT_VICMD_SYMBOL=">"
     PIPENV_ACTIVE_COLOR=7
-"##,
+"#,
                 )
                 .arg(Arg::new("last_return_code").short('r').takes_value(true))
                 .arg(Arg::new("keymap").short('k').takes_value(true))
